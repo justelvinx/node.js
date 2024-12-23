74d63d4051060df1fbd3051a4106146f628f211c
root <root@LAPTOP-S84B1CR6.>
2024-12-23 11:31:31 +0400

    Update greeting message

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const http = require('http');
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, DevOps World!\n');
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello, CI/CD World!\n'); // Changed message
 });
 
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
 
